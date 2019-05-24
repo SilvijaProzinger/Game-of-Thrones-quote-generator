b93a313db0893a7662611cf8c9f76fbd826501bb
Silvija <silvija.prozinger@gmail.com>
2019-05-24 15:32:55 +0200

    added favorite quotes div which saves quotes marked as favorite

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var favorite;
 
 /* array with quotes */
 var quotes = [
-'"A mind needs books as a sword needs a whetstone, if it is to keep its edge."', 
+'"A mind needs books as a sword needs a whetstone, if it is to keep its edge."',
 '"Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you."',
 '"Fear cuts deeper than swords."', '"Winter is coming."', '"When you play a game of thrones you win or you die."', '"Some old wounds never truly heal, and bleed again at the slightest word."',
 '"Most men would rather deny a hard truth than face it"','"Death is so terribly final, while life is full of possibilities."'
@@ -26,7 +26,7 @@ function addFavorite(){
 	console.log('click');
 	var favorite = quote.innerHTML;
 	favoriteQuotes.push(favorite);
-	console.log(favorite);	
+	console.log(favorite);
 }
 
 savedFavesButton.addEventListener('click', showFavorite);
@@ -34,6 +34,14 @@ savedFavesButton.addEventListener('click', showFavorite);
 function showFavorite(){
 	console.log('click');
 	var favoritesList = document.getElementById('faves-content');
-	favoritesList.innerHTML = favoriteQuotes + '\n';
-	favoritesList.style.display = 'block';
+	if (favoriteQuotes == '') {
+		favoritesList.style.display = 'none';
+	}
+	else {
+		favoritesList.innerHTML = favoriteQuotes;
+		favoritesList.style.display = 'block';
+		setTimeout(function(){
+			favoritesList.style.display = 'none';
+		},3000);
+	}	
 }
